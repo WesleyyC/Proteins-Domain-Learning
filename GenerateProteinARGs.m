a9dfe21cd1658310f4c9922ff2d7ffa429b4528c
wesleyyc <wesley.chin0919@gmail.com>
2015-09-04 10:09:35 -0400

    some promising result

--- GenerateProteinARGs.m
+++ GenerateProteinARGs.m
@@ -1,77 +1,30 @@
-%% A script for generating proteins ARG
-
-
-%% Set Up Variable
-
-BLOSUM_Sigma = 2;
-
-distance_cutoff = 12;
-
-start_sequence = 1;
-
-end_sequence = 50;
-
-
-%% Generate BLOSSUM
-
-C=[9,-1,-1,-3,0,-3,-3,-3,-4,-3,-3,-3,-3,-1,-1,-1,-1,-2,-2,-2];
-S=[-1,4,1,-1,1,0,1,0,0,0,-1,-1,0,-1,-2,-2,-2,-2,-2,-3];
-T=[-1,1,4,1,-1,1,0,1,0,0,0,-1,0,-1,-2,-2,-2,-2,-2,-3];
-P=[-3,-1,1,7,-1,-2,-1,-1,-1,-1,-2,-2,-1,-2,-3,-3,-2,-4,-3,-4];
-A=[0,1,-1,-1,4,0,-1,-2,-1,-1,-2,-1,-1,-1,-1,-1,-2,-2,-2,-3];
-G=[-3,0,1,-2,0,6,-2,-1,-2,-2,-2,-2,-2,-3,-4,-4,0,-3,-3,-2];
-N=[-3,1,0,-2,-2,0,6,1,0,0,-1,0,0,-2,-3,-3,-3,-3,-2,-4];
-D=[-3,0,1,-1,-2,-1,1,6,2,0,-1,-2,-1,-3,-3,-4,-3,-3,-3,-4];
-E=[-4,0,0,-1,-1,-2,0,2,5,2,0,0,1,-2,-3,-3,-3,-3,-2,-3];
-Q=[-3,0,0,-1,-1,-2,0,0,2,5,0,1,1,0,-3,-2,-2,-3,-1,-2];
-H=[-3,-1,0,-2,-2,-2,1,1,0,0,8,0,-1,-2,-3,-3,-2,-1,2,-2];
-R=[-3,-1,-1,-2,-1,-2,0,-2,0,1,0,5,2,-1,-3,-2,-3,-3,-2,-3];
-K=[-3,0,0,-1,-1,-2,0,-1,1,1,-1,2,5,-1,-3,-2,-3,-3,-2,-3];
-M=[-1,-1,-1,-2,-1,-3,-2,-3,-2,0,-2,-1,-1,5,1,2,-2,0,-1,-1];
-I=[-1,-2,-2,-3,-1,-4,-3,-3,-3,-3,-3,-3,-3,1,4,2,1,0,-1,-3];
-L=[-1,-2,-2,-3,-1,-4,-3,-4,-3,-2,-3,-2,-2,2,2,4,3,0,-1,-2];
-V=[-1,-2,-2,-2,0,-3,-3,-3,-2,-2,-3,-3,-2,1,3,1,4,-1,-1,-3];
-F=[-2,-2,-2,-4,-2,-3,-3,-3,-3,-3,-1,-3,-3,0,0,0,-1,6,3,1];
-Y=[-2,-2,-2,-3,-2,-3,-2,-3,-2,-1,2,-2,-2,-1,-1,-1,-1,3,7,2];
-W=[-2,-3,-3,-4,-3,-2,-4,-4,-3,-2,-2,-3,-3,-1,-3,-2,-3,1,2,11];
-
-BLOSUM=[C',S',T',P',A',G',N',D',E',Q',H',R',K',M',I',L',V',F',Y',W'];
-
-BLOSUM=exp(BLOSUM/BLOSUM_Sigma);
-
-% normalize the symmetric matrix
-% Not perfect
-s=sum(BLOSUM,2);
-n=repmat(s,1,20);
-BLOSUM=BLOSUM./n;
-
-%% Input the File
-
-protein = csvread('new_4D1E.csv');
-protein = protein(start_sequence:end_sequence,:);
-number_of_AA = size(protein,1);
-
-%% Ready for ARGs
-
-proteinStructure = zeros(number_of_AA);
-
-proteinAtrs = zeros([number_of_AA,1]);
-
-for i = 1:number_of_AA
+function [ proteinARG ] = GenerateProteinARGs( start_sequence,end_sequence, fileName,distance_cutoff)
     
-    proteinAtrs(i) = protein(i,2);
-    
-    for j = i+1:number_of_AA
-        x1=protein(i,3);
-        y1=protein(i,4);
-        z1=protein(i,5);
-        x2=protein(j,3);
-        y2=protein(j,4);
-        z2=protein(j,5);
-        dist = sqrt((x1-x2)^2+(y1-y2)^2+(z1-z2)^2);
-        dist = dist*(dist<=distance_cutoff);
-        proteinStructure(i,j)=dist;
-        proteinStructure(j,i)=dist;
+    protein = csvread(fileName);
+    protein = protein(start_sequence:end_sequence,:);
+    number_of_AA = size(protein,1);
+
+    proteinStructure = zeros(number_of_AA);
+
+    proteinAtrs = zeros([number_of_AA,1]);
+
+    for i = 1:number_of_AA
+
+        proteinAtrs(i) = protein(i,2);
+
+        for j = i+1:number_of_AA
+            x1=protein(i,3);
+            y1=protein(i,4);
+            z1=protein(i,5);
+            x2=protein(j,3);
+            y2=protein(j,4);
+            z2=protein(j,5);
+            dist = sqrt((x1-x2)^2+(y1-y2)^2+(z1-z2)^2);
+            dist = dist*(dist<=distance_cutoff);
+            proteinStructure(i,j)=dist;
+            proteinStructure(j,i)=dist;
+        end
     end
+    
+    proteinARG = ARG(proteinStructure,proteinAtrs);
 end
-
---
+++ protein_match.m
@@ -0,0 +1,69 @@
+%% Script for matching the protein
+
+
+%% Set Up Edge and Node Attribute
+
+BLOSUM_Sigma = 2;   % node attribute
+
+distance_cutoff = 13;   % edge attribute cut off
+
+%% Set Up Protein
+
+proteinOneFile = 'new_4Q59.csv';
+start_sequence_one = 1;
+end_sequence_one = 50;
+
+proteinTwoFile = 'new_4D1E.csv';
+start_sequence_two = 1;
+end_sequence_two = 60;
+
+proteinOneARG = GenerateProteinARGs(start_sequence_one,end_sequence_one, proteinOneFile,distance_cutoff);
+proteinTwoARG = GenerateProteinARGs(start_sequence_two,end_sequence_two, proteinTwoFile,distance_cutoff);
+
+
+%% Generate BLOSSUM
+
+C=[9,-1,-1,-3,0,-3,-3,-3,-4,-3,-3,-3,-3,-1,-1,-1,-1,-2,-2,-2];
+S=[-1,4,1,-1,1,0,1,0,0,0,-1,-1,0,-1,-2,-2,-2,-2,-2,-3];
+T=[-1,1,4,1,-1,1,0,1,0,0,0,-1,0,-1,-2,-2,-2,-2,-2,-3];
+P=[-3,-1,1,7,-1,-2,-1,-1,-1,-1,-2,-2,-1,-2,-3,-3,-2,-4,-3,-4];
+A=[0,1,-1,-1,4,0,-1,-2,-1,-1,-2,-1,-1,-1,-1,-1,-2,-2,-2,-3];
+G=[-3,0,1,-2,0,6,-2,-1,-2,-2,-2,-2,-2,-3,-4,-4,0,-3,-3,-2];
+N=[-3,1,0,-2,-2,0,6,1,0,0,-1,0,0,-2,-3,-3,-3,-3,-2,-4];
+D=[-3,0,1,-1,-2,-1,1,6,2,0,-1,-2,-1,-3,-3,-4,-3,-3,-3,-4];
+E=[-4,0,0,-1,-1,-2,0,2,5,2,0,0,1,-2,-3,-3,-3,-3,-2,-3];
+Q=[-3,0,0,-1,-1,-2,0,0,2,5,0,1,1,0,-3,-2,-2,-3,-1,-2];
+H=[-3,-1,0,-2,-2,-2,1,1,0,0,8,0,-1,-2,-3,-3,-2,-1,2,-2];
+R=[-3,-1,-1,-2,-1,-2,0,-2,0,1,0,5,2,-1,-3,-2,-3,-3,-2,-3];
+K=[-3,0,0,-1,-1,-2,0,-1,1,1,-1,2,5,-1,-3,-2,-3,-3,-2,-3];
+M=[-1,-1,-1,-2,-1,-3,-2,-3,-2,0,-2,-1,-1,5,1,2,-2,0,-1,-1];
+I=[-1,-2,-2,-3,-1,-4,-3,-3,-3,-3,-3,-3,-3,1,4,2,1,0,-1,-3];
+L=[-1,-2,-2,-3,-1,-4,-3,-4,-3,-2,-3,-2,-2,2,2,4,3,0,-1,-2];
+V=[-1,-2,-2,-2,0,-3,-3,-3,-2,-2,-3,-3,-2,1,3,1,4,-1,-1,-3];
+F=[-2,-2,-2,-4,-2,-3,-3,-3,-3,-3,-1,-3,-3,0,0,0,-1,6,3,1];
+Y=[-2,-2,-2,-3,-2,-3,-2,-3,-2,-1,2,-2,-2,-1,-1,-1,-1,3,7,2];
+W=[-2,-3,-3,-4,-3,-2,-4,-4,-3,-2,-2,-3,-3,-1,-3,-2,-3,1,2,11];
+
+BLOSUM=[C',S',T',P',A',G',N',D',E',Q',H',R',K',M',I',L',V',F',Y',W'];
+
+BLOSUM=exp(BLOSUM/BLOSUM_Sigma);
+
+% normalize the symmetric matrix
+% Not perfect
+s=sum(BLOSUM,2);
+n=repmat(s,1,20);
+BLOSUM=BLOSUM./n;
+
+%% Match the prote
+
+match = graph_matching(proteinOneARG, proteinTwoARG,BLOSUM);
+figure; imshow(match*2);
+% 
+% %% Save the result
+% datetime=datestr(now);
+% datetime=strrep(datetime,':','_'); %Replace colon with underscore
+% datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
+% datetime=strrep(datetime,' ','_');%Replace space with underscore
+% 
+% save (datetime)
+
